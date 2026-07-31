41f2a896d043191b263c08b85085cfc3ebba8086
DilushaFdo <130301311+DilushaFdo@users.noreply.github.com>
2026-07-31 13:23:39 +0530

    backend files added

---
+++ backend/train-seat-booking-app/src/main/java/com/example/train_seat_booking_app/TrainSeatBookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.train_seat_booking_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainSeatBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainSeatBookingAppApplication.class, args);
+	}
+
+}
